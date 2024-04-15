c02ee37f0c47dc8c27fefa33887540c7b794fe80
Chekhanadski Andrei <chekhanadski@gmail.com>
2024-04-15 03:21:23 +0200

    feat: solve retry task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -161,8 +161,18 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return function retryFunction(...args) {
+    let lastError;
+    for (let i = 0; i <= attempts; i += 1) {
+      try {
+        return func(...args);
+      } catch (error) {
+        lastError = error;
+      }
+    }
+    throw lastError;
+  };
 }
 
 /**
